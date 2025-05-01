c7d9ef3412ecc8d817a0797a81a81cd9d2a9e406
salva <salvamanu12@gmail.com>
2025-05-01 17:20:04 -0300

    feat: :sparkles: Add a new meeting.

--- Server/controllers/meeting/_routes.js
+++ Server/controllers/meeting/_routes.js
@@ -6,5 +6,6 @@ const auth = require('../../middelwares/auth');
 const router = express.Router();
 
 router.get('/', auth, meeting.index)
+router.post('/add', auth, meeting.add)
 
 module.exports = router
--- Server/controllers/meeting/meeting.js
+++ Server/controllers/meeting/meeting.js
@@ -2,13 +2,20 @@ const MeetingHistory = require('../../model/schema/meeting')
 const mongoose = require('mongoose');
 
 const add = async (req, res) => {
-   
+    try {
+        req.body.createdDate = new Date();
+        const user = new MeetingHistory(req.body);
+        await user.save();
+        res.status(200).json(user);
+    } catch (err) {
+        console.error('Failed to create MeetingHistory:', err);
+        res.status(400).json({ error: 'Failed to create MeetingHistory' });
+    }
 }
 
 const index = async (req, res) => {
     const query = req.query
     query.deleted = false;
-    // let result = await Property.find(query)
     let allData = await MeetingHistory.find(query).populate({
         path: 'createBy',
         match: { deleted: false } // Populate only if createBy.deleted is false
