55d926a36d1641e79f548b822a6c848e51fb8182
salva <salvamanu12@gmail.com>
2025-05-01 17:38:32 -0300

    feat: :sparkles: Delete many meetings.

--- Server/controllers/meeting/_routes.js
+++ Server/controllers/meeting/_routes.js
@@ -10,5 +10,6 @@ router.post('/add', auth, meeting.add)
 router.get('/view/:id', auth, meeting.view)
 router.put('/edit/:id', auth, meeting.edit)
 router.delete('/delete/:id', auth, meeting.deleteData)
+router.post('/deleteMany', auth, meeting.deleteMany)
 
 module.exports = router
--- Server/controllers/meeting/meeting.js
+++ Server/controllers/meeting/meeting.js
@@ -56,7 +56,12 @@ const deleteData = async (req, res) => {
 }
 
 const deleteMany = async (req, res) => {
-    
+    try {
+        const meeting = await MeetingHistory.updateMany({ _id: { $in: req.body } }, { $set: { deleted: true } });
+        res.status(200).json({ message: "done", meeting })
+    } catch (err) {
+        res.status(404).json({ message: "error", err })
+    }
 }
 
 module.exports = { add, index, view, deleteData, deleteMany, edit }
